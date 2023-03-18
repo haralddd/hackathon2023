ce4156ff853bd5c1ebd9b78519e29fbe235e8b73
haralg <haralbbj@stud.ntnu.no>
2023-03-18 01:33:41 +0100

    Added julia files and updated readme. Dataset with fall occurrences.

--- publish.jl
+++ publish.jl
@@ -1,16 +1,7 @@
-# hackathon2023
-NORNIR Hackathon 2023 Synx HIVE IoT
+using Pkg; Pkg.add("CSV"); Pkg.add("DataFrames"); Pkg.add("HTTP"); Pkg.add("JSON"); Pkg.add("JSONTables")
+using HTTP, CSV, DataFrames, JSON, JSONTables
 
-# WARNING!
-Hard coded access token is a free access token for the case. Do not use it for sensitive data.
-
-# Data set
-
-Pulled from https://www.kaggle.com/code/shuklaop/notebook73116787a0
-
-Description:
-"""
-
+#= 
 Though there is a good number of wearables in the market that provide care for older adults, most of them only focus on fall detection and not prediction. Some wearables proposed are able to detect trips and slips but not specifically fall. So, cStick, a calm stick to monitor falls in older adults is proposed. cStick has been designed in such a way that it can assist both visually and hearing-impaired older adults. cStick has an ability to not only detect falls but to also predict the incident of fall so as to reduce their occurrences. cStick can monitor the surroundings, warn the user if there was a previous fall detected at a certain location, and update the location and its surroundings to the user. Based on the changes in the monitored parameters, the decision of fall i.e., a prediction, warning or detection of fall is made with an accuracy of approximately 95%.
 
 The cStick.csv file that is presented here has parameters- distance, pressure (0-small, 1- medium and 2- high pressures), HRV, Sugar Levels, SpO2 levels and accelerometer reading (<+-3g, i.e., threshold is 0 and >Threshold is 1) in relationship to the decision of falls (0- no fall detected, 1- person slipped/tripped/prediction of fall and 2- definite fall)
@@ -20,5 +11,43 @@ L. Rachakonda, A. Sharma, S. P. Mohanty, and E. Kougianos, "Good-Eye: A Combined
 L. Rachakonda, S. P. Mohanty, and E. Kougianos, “Good-Eye: A Device for Automatic Prediction and Detection of Elderly Falls in Smart Homes”, in Proceedings of the 6th IEEE International Symposium on Smart Electronic Systems (iSES), 2020, pp. 202--203.
 L. Rachakonda, S. P. Mohanty, and E. Kougianos, “cStick: A Calm Stick for Fall Prediction, Detection and Control in the IoMT Framework”, in Proceedings of the 4th IFIP International Internet of Things (IoT) Conference (IFIP-IoT), 2021, pp. Accepted on 02 Sep 2021.
 
-"""
+=#
+
+
+headers = [
+    "Synx-Cat" => "1",
+    "Content-Type" => "application/x-www-form-urlencoded",
+]
+df = DataFrame(CSV.File("cStick.csv"))
+
+
+
+size_lim = 1024 # 1 KB
+num_rows = size(df, 1)
+num_cols = size(df, 2)
+max_datarate = 150_000 # 150 KB/s
+wait_time = size_lim / max_datarate
+
+df_size = num_rows * num_cols * 8 # 8 bytes per Float64
+
+@show chunk_size = floor(Int, size_lim / (num_cols * 8))
+
+
+
+# Send HTTP POST
+service_url = "https://group5.cioty.com/health-status"
+base_body = "token=aToken_36d8715e3531fd8e8c01fcbfd26bf5af1908e14f15014d2d14817b568bc0bb0e&objectID=1"
+
+for i in 1:chunk_size:num_rows
+    chunk = df[i:min(i+chunk_size-1, num_rows), :]
+    data_send = base_body * "&data=" * string(objecttable(chunk))
+    display(data_send)
+    resp = HTTP.post("https://group5.cioty.com/health-status"; headers = headers, body = data_send, verify=false)
+    sleep(wait_time)
+end
+
+data_send = base_body * "&data=1203023010.0000"
+resp = HTTP.post("https://group5.cioty.com/health-status"; headers = headers, body = data_send, verify=false)
+
+
 
